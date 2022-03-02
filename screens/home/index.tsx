cb422bbcb3156105abfa7ed1f3eedd36f333781d
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-02 19:40:22 -0300

    Create index.tsx

---
+++ screens/home/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, StatusBar } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <StatusBar backgroundColor={"#fff"} barStyle="dark-content" />
+      <Text> Home </Text>
+    </View>
+  );
+};
+
+export default Home;
