bffd0ce3a81cf0afd63112fdda58583516a0d3c1
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-03 18:26:16 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,28 @@
+import {
+  useFonts,
+  Comfortaa_300Light,
+  Comfortaa_400Regular,
+  Comfortaa_500Medium,
+  Comfortaa_600SemiBold,
+  Comfortaa_700Bold,
+} from "@expo-google-fonts/comfortaa";
+import AppLoading from "expo-app-loading";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Routes from "./routes";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Comfortaa_300Light,
+    Comfortaa_400Regular,
+    Comfortaa_500Medium,
+    Comfortaa_600SemiBold,
+    Comfortaa_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <SafeAreaProvider>
       <Routes />
