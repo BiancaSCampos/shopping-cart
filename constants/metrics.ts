12311a3dc90eef6cb03e64bfdeb4800e7fbe1834
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-03 18:26:26 -0300

    Update metrics.ts

--- constants/metrics.ts
+++ constants/metrics.ts
@@ -1,5 +1,28 @@
 import { Platform, StatusBar } from "react-native";
 
 const metrics = {
+  text: {
+    fontSize: {
+      small: 20,
+      medium: 30,
+      large: 40,
+      xlarge: 50,
+    },
+
+    fontFamily: {
+      light: "Comfortaa_300Light",
+      regular: "Comfortaa_400Regular",
+      medium: "Comfortaa_500Medium",
+      semiBold: "Comfortaa_600SemiBold",
+      bold: "Comfortaa_700Bold",
+    },
+  },
+
+  padding: {
+    small: 30,
+    medium: 60,
+    large: 90,
+  },
+
   statusBar: Platform.OS === "android" ? `_${StatusBar.currentHeight}` : 0,
 };
