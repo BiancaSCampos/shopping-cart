bcd71e3048094c90a08fbb8565a83ba29bc6a30b
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-04 16:48:53 -0300

    Create styles.ts

---
+++ components/atoms/avatar/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "pr-unit";
+
+const styles = StyleSheet.create({
+  container: {
+    height: 200,
+    width: 200,
+    borderRadius: 300,
+    elevation: 10,
+  },
+
+  image: {
+    height: "100%",
+    width: "100%",
+    borderRadius: 300,
+    borderWidth: 2,
+    borderColor: "#fff",
+  },
+});
+
+export default styles;
