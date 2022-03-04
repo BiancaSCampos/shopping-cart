1a447b42eedc076b3a1defcb7d2d15f2119fd91a
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-04 16:48:55 -0300

    Create index.tsx

---
+++ components/atoms/brandFilter/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const BrandFilter = () => {
+  return (
+    <View>
+      <Text>BrandFilter</Text>
+    </View>
+  );
+};
+
+export default BrandFilter;
