7e89f0d098ede61fab38ebc6973128b63e45d27e
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-07 20:25:17 -0300

    Update styles.ts

--- components/atoms/brandFilter/styles.ts
+++ components/atoms/brandFilter/styles.ts
@@ -1,8 +1,21 @@
 import { StyleSheet } from "pr-unit";
+import colors from "../../../constants/colors";
+import metrics from "../../../constants/metrics";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: 175,
+    width: 175,
+
+    padding: metrics.spacers.small,
+
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: metrics.borderRadius.large,
+
+    backgroundColor: colors.primary,
+
+    marginTop: metrics.spacers.medium,
   },
 });
 
