25195c91e40f3af42ef937145fe2c09f28c13256
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-07 20:25:15 -0300

    Update index.tsx

--- components/atoms/brandFilter/index.tsx
+++ components/atoms/brandFilter/index.tsx
@@ -1,12 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { AdidasSymbol, NikeSymbol, PumaSymbol } from "../../../assets/icons";
+import styles from "./styles";
 
-const BrandFilter = () => {
-  return (
-    <View>
-      <Text>BrandFilter</Text>
-    </View>
-  );
+const BrandFilter = ({
+  children,
+}: {
+  children: React.ReactNode | React.ReactNode[];
+}) => {
+  return <View style={styles.container}>{children}</View>;
 };
 
 export default BrandFilter;
