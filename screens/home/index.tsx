603a62b36126cbef5892781c583eb7268de1e510
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-08 14:58:53 -0300

    Update index.tsx

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StatusBar, Image } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 import React from "react";
 import Avatar from "../../components/atoms/avatar";
 import styles from "./styles";
@@ -14,10 +14,11 @@ const Home = () => {
         <Avatar />
 
         <Text style={styles.text}>Catalog</Text>
-
-        <BrandFilter children={<NikeSymbol />} />
-        <BrandFilter children={<AdidasSymbol />} />
-        <BrandFilter children={<PumaSymbol />} />
+        <View style={styles.filtersContainer}>
+          <BrandFilter icon={<NikeSymbol width={40} height={18} />} />
+          <BrandFilter icon={<AdidasSymbol width={40} height={30} />} />
+          <BrandFilter icon={<PumaSymbol width={40} height={50} />} />
+        </View>
       </View>
     </>
   );
