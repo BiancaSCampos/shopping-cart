5146309d5037ed0413adc021d04bca68af22df1c
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-09 16:52:30 -0300

    Update styles.ts

--- screens/home/styles.ts
+++ screens/home/styles.ts
@@ -4,9 +4,15 @@ import metrics from "../../constants/metrics";
 
 const styles = StyleSheet.create({
   container: {
-    padding: metrics.spacers.small,
+    padding: metrics.spacers.medium,
     backgroundColor: colors.secondary,
     flex: 1,
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 
   text: {
@@ -20,6 +26,12 @@ const styles = StyleSheet.create({
   filtersContainer: {
     flexDirection: "row",
     width: "100%",
+    justifyContent: "space-between",
+    marginBottom: metrics.spacers.medium,
+  },
+
+  productsContainer: {
+    flexDirection: "row",
     justifyContent: "space-between",
   },
 });
