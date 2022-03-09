7062c93f2e2c9c0086f9d275e7ac9968e0bb3b08
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-09 16:52:22 -0300

    Update index.tsx

--- components/atoms/product/index.tsx
+++ components/atoms/product/index.tsx
@@ -1,11 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import styles from "./styles";
+import { AddCart, Favorite } from "../../../assets/icons";
+import colors from "../../../constants/colors";
 
 const Product = () => {
   return (
-    <View>
+    <TouchableOpacity style={styles.container}>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity>
+          <Favorite fill={colors.disabled} width={25} height={25} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <AddCart width={25} height={25} />
+        </TouchableOpacity>
+      </View>
+      <Image
+        source={{
+          uri: "https://imgcentauro-a.akamaihd.net/230x230/95195931.jpg",
+        }}
+        style={{ width: 100, height: 100 }}
+      />
       <Text>Product</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
