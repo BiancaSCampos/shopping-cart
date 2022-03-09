362d291f8b241e9a7dfb00742b3ac948c9352e71
BiancaSilvaC <biancascampos0410@gmail.com>
2022-03-09 16:52:28 -0300

    Update index.tsx

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -1,23 +1,39 @@
 import { View, Text, StatusBar } from "react-native";
 import React from "react";
-import Avatar from "../../components/atoms/avatar";
 import styles from "./styles";
 import colors from "../../constants/colors";
+
+import Avatar from "../../components/atoms/avatar";
 import BrandFilter from "../../components/atoms/brandFilter";
-import { AdidasSymbol, NikeSymbol, PumaSymbol } from "../../assets/icons";
+import {
+  AdidasSymbol,
+  NikeSymbol,
+  PumaSymbol,
+  Search,
+} from "../../assets/icons";
+import Product from "../../components/atoms/product";
 
 const Home = () => {
   return (
     <>
       <StatusBar backgroundColor={colors.secondary} barStyle="light-content" />
       <View style={styles.container}>
-        <Avatar />
+        <View style={styles.header}>
+          <Avatar />
+          <Search height={30} width={30} />
+        </View>
 
         <Text style={styles.text}>Catalog</Text>
+
         <View style={styles.filtersContainer}>
           <BrandFilter icon={<NikeSymbol width={40} height={18} />} />
           <BrandFilter icon={<AdidasSymbol width={40} height={30} />} />
           <BrandFilter icon={<PumaSymbol width={40} height={50} />} />
+        </View>
+
+        <View style={styles.productsContainer}>
+          <Product />
+          <Product />
         </View>
       </View>
     </>
